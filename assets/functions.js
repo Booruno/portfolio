55c2358c5a61d37fb79257d49ba7868de802f8cc
Booruno <1brunoocunha@gmail.com>
2022-04-02 21:36:05 -0300

    finalizações

--- assets/functions.js
+++ assets/functions.js
@@ -15,3 +15,34 @@ tabs.forEach((tab) => {
     tab.classList.add("filtro-tab-ativo");
   });
 });
+
+// Temas
+const themeButton = document.getElementById("tema-btn");
+const darkTheme = "dark-mode";
+const iconTheme = "bi bi-brightness-hig";
+
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme)
+    ? "bi bi-moon-stars-fill"
+    : "bi bi-brightness-hig";
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[
+    selectedIcon === "bi bi-moon-stars-fill" ? "add" : "remove"
+  ](iconTheme);
+}
+
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
